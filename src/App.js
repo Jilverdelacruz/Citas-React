9a4ffe96805c919a9f12bc606a4b261d1169955b
Jilverdelacruz <jilver.practicando@gmail.com>
2022-07-03 21:32:19 -0500

    Uso del local Storage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Header from "./Components/Header";
 import Formulario from "./Components/Formulario";
 import ListadoPacientes from "./Components/ListadoPacientes";
-import {useState} from "react"
+import {useState, useEffect} from "react"
 
 function App() {
 
-  const [pacientes, setPacientes] = useState([])
+  const [pacientes, setPacientes] = useState(JSON.parse(localStorage.getItem('pacientes')) ?? []) // Al momento de inicializar lo primero que hará es bajarse todo lo que tiene en local Storage, ?? es un operador lógico (Nullish coalescing operator (??) que indica si en caso el lado izquierdo es nulo lo inicilizas como arreglo vacío, esto se hace en caso de alguien elimine el local storage, cuando recargues se pondrá nulo inmediatamente el operador lógico lo pondrá como arreglo
   const [paciente, setPaciente] = useState({})
 
   const eliminarPaciente = id =>{
@@ -13,6 +13,13 @@ function App() {
     const pacientesActualizados = pacientes.filter(paciente => paciente.id !== id)
     setPacientes(pacientesActualizados)
   }
+
+
+// usar el local storage para que no se elimine los datos al recargar las pestañas
+
+useEffect(()=>{  // sincronizamos para que cuando tenga data mi arreglo su pase al local storage
+  localStorage.setItem('pacientes', JSON.stringify(pacientes))
+}, [pacientes])
 
   return (
     <div className="container mx-auto mt-20"> 
